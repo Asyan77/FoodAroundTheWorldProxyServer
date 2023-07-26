e7a242ee5a4d36519697f1dcc48a2b6b21bce510
Ashley Yan <Asyan77@gmail.com>
2023-07-26 14:30:32 -0700

    replaced index.js with copy/paste from TravelTables API page

--- index.js
+++ index.js
@@ -11,22 +11,50 @@ const app = express();
 app.use(cors());
 
 // to bypass CORS
-app.get('/', async (req, res) => {
-  if (req.query.url) {
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': process.env.API_KEY,
-        'X-RapidAPI-Host': 'cost-of-living-and-prices.p.rapidapi.com'
-      }
+// app.get('/', async (req, res) => {
+//   if (req.query.url) {
+//     const options = {
+//       method: 'GET',
+//       headers: {
+//         'X-RapidAPI-Key': process.env.API_KEY,
+//         'X-RapidAPI-Host': 'cost-of-living-and-prices.p.rapidapi.com'
+//       },
+//       url: 'https://cost-of-living-and-prices.p.rapidapi.com/prices',
+//       params: {
+//         city_name: 'Bratislava',
+//         country_name: 'Slovakia'
+//       },
+//     }
+//     const proxyRes = await axios.get(`${req.query.url}`, options );
+//     // const proxyRes = await axios.get(req.query.url);
+//     res.send(proxyRes.data);
+//   } else {
+//     res.send('Please provide a url query parameter');
+//   }
+// });
+
+app.get('/',async (req, res) => {
+  const options = {
+    method: 'GET',
+    url: 'https://cost-of-living-and-prices.p.rapidapi.com/prices',
+    params: {
+      city_name: 'Bratislava',
+      country_name: 'Slovakia'
+    },
+    headers: {
+      'X-RapidAPI-Key': 'e377e81e46mshc55f3ddf23c5d2dp1a6d0cjsn8fe11bcbb1f8',
+      'X-RapidAPI-Host': 'cost-of-living-and-prices.p.rapidapi.com'
     }
-    const proxyRes = await axios.get(`${req.query.url}`, options );
-    // const proxyRes = await axios.get(req.query.url);
-    res.send(proxyRes.data);
-  } else {
-    res.send('Please provide a url query parameter');
+  };
+  
+  try {
+    const response = await axios.request(options);
+    console.log(response.data);
+    return response.data
+  } catch (error) {
+    console.error(error);
   }
-});
+})
 
 app.listen(5001, () => {
   console.log('Listening on port 5001...');
