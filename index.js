68377650e7a02f26ca5c4457e6c57cf242823356
Ashley Yan <Asyan77@gmail.com>
2023-07-26 11:42:04 -0700

    update with api key

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV !== 'production') {
   const dotenv = require('dotenv');
   dotenv.config();
 }
@@ -13,8 +13,15 @@ app.use(cors());
 // to bypass CORS
 app.get('/', async (req, res) => {
   if (req.query.url) {
-    // const proxyRes = await axios.get(`${req.query.url}&apiKey=${process.env.API_KEY}`);
-    const proxyRes = await axios.get(req.query.url);
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': process.env.API_KEY,
+        'X-RapidAPI-Host': 'cost-of-living-and-prices.p.rapidapi.com'
+      }
+    }
+    const proxyRes = await axios.get(`${req.query.url}`, options );
+    // const proxyRes = await axios.get(req.query.url);
     res.send(proxyRes.data);
   } else {
     res.send('Please provide a url query parameter');
